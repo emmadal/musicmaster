a11705d6450583ca4959e5a8d769715d5fbd0489
emmadal <emmanueldalougou@gmail.com>
2019-06-17 18:50:10 +0200

    Updates

---
+++ precache-manifest.f7ad80896f74cfc17b0a4c96de1c4f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1b149e968b87d3d804a04aed9a3c0a",
+    "url": "/musicmaster/index.html"
+  },
+  {
+    "revision": "8eb59c5e131b9f410840",
+    "url": "/musicmaster/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "be41a961cd9ab188f178",
+    "url": "/musicmaster/static/css/main.1aaac61c.chunk.css"
+  },
+  {
+    "revision": "8eb59c5e131b9f410840",
+    "url": "/musicmaster/static/js/2.09cd3c22.chunk.js"
+  },
+  {
+    "revision": "be41a961cd9ab188f178",
+    "url": "/musicmaster/static/js/main.a3d806a0.chunk.js"
+  },
+  {
+    "revision": "efee245af07170807616",
+    "url": "/musicmaster/static/js/runtime~main.ca7838c9.js"
+  }
+]);
